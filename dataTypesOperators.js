34851f78fbbc77a0d2e9c6304238de16e6e3ec05
Emmanuel Tofa <emmanuel.t@m365connect.com>
2024-06-11 08:40:22 +0200

    Data types

---
+++ dataTypesOperators.js
@@ -0,0 +1,47 @@
+// Exercise 1: Numbers
+let number1 = 20;
+let floatingNumber = 20.5;
+
+let addition = number1 + floatingNumber;
+let subtraction = number1 - floatingNumber;
+let multiplication = number1 * floatingNumber;
+let division = number1 / floatingNumber;
+let modulus = number1 % 3;
+let exponentiation = number1 ** 2;
+
+console.log(number1, floatingNumber, addition, subtraction, multiplication, division, modulus, exponentiation);
+
+// Exercise 2: Boolean and Operators
+let comparison1 = number1 > floatingNumber;
+let comparison2 = number1 <= floatingNumber;
+let comparison3 = number1 == floatingNumber;
+let comparison4 = number1 != floatingNumber;
+
+console.log(comparison1, comparison2, comparison3, comparison4);
+
+let x = 8;
+let y = 12;
+
+console.log(x > y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x != y);
+
+let a = true;
+let b = false;
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+let p = 10;
+p += 5;
+console.log(p);
+p -= 2;
+console.log(p);
+p *= 3;
+console.log(p);
+p /= 2;
+console.log(p);
+p %= 3;
+console.log(p);
