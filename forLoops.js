d8d191b9d49c1d0c91ad71c0329d88c9ab16bd5f
Emmanuel Tofa <emmanuel.t@m365connect.com>
2024-06-11 08:55:07 +0200

    While loops

--- forLoops.js
+++ forLoops.js
@@ -22,12 +22,4 @@ for (let i = 0; i < numbers1.length; i++) {
     console.log(numbers1[i]);
 }
 
-// 5. Find and print the largest number
-const numbers2 = [3, 7, 2, 5, 10, 6];
-let max = numbers2[0];
-for (let i = 1; i < numbers2.length; i++) {
-    if (numbers2[i] > max) {
-        max = numbers2[i];
-    }
-}
-console.log(max);
+
---
+++ whileLoops.js
@@ -0,0 +1,30 @@
+// Exercise 4: While loops
+// 1. Print numbers from 1 to 10
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+// 2. Print all even numbers between 1 and 20
+i = 2;
+while (i <= 20) {
+    console.log(i);
+    i += 2;
+}
+
+// 3. Calculate the sum of all numbers from 1 to 100 and print the result
+let sum = 0;
+i = 1;
+while (i <= 100) {
+    sum += i;
+    i++;
+}
+console.log(sum);
+
+// 4. Print all multiples of 5 less than 50
+i = 5;
+while (i < 50) {
+    console.log(i);
+    i += 5;
+}
