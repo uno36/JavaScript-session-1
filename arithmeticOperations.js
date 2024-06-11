9cd35acd3c5bb534557ebda601d021bb7f49b9b1
Emmanuel Tofa <emmanuel.t@m365connect.com>
2024-06-11 08:27:46 +0200

    Arithmetics

---
+++ arithmeticOperations.js
@@ -0,0 +1,13 @@
+// Exercise 3: Arithmetic Operations
+let num1 = 10; 
+let num2 = 5;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
